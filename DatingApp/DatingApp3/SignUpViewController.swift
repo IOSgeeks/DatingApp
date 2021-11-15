ec599f8832ae018e39f7b2b4a6bea1e9fab200de
Catalina Bedoya <78061240+CatalinaBedoya1@users.noreply.github.com>
2021-11-15 16:29:40 -0500

    update

--- DatingApp/DatingApp3/SignUpViewController.swift
+++ DatingApp/DatingApp3/SignUpViewController.swift
@@ -2,11 +2,28 @@
 //  SignUpViewController.swift
 //  DatingApp3
 //
-//  Created by Jessie Lin on 11/15/21.
+//  Created by Catalina Bedoya on 11/15/21.
 //
 
 import UIKit
 
-class SignUpViewController: UILoginTableViewCell {
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
