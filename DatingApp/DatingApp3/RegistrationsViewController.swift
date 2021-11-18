74cd4b742f89c260d0ef9e37ee9a9499d50b951e
Wildline <Wildlinelincifort11234@gmail.com>
2021-11-18 12:13:02 -0500

    Update with Registration VC

---
+++ DatingApp/DatingApp3/RegistrationsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RegistrationsViewController.swift
+//  DatingApp3
+//
+//  Created by Wildline  Lincifort on 11/18/21.
+//
+
+import UIKit
+
+class RegistrationsViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet weak var FullNameTxt: UITextField!
+    
+    @IBOutlet weak var BirthDateTxt: UITextField!
+    
+    @IBOutlet weak var GenderTxt: UITextField!
+    
+    @IBOutlet weak var SexualOriTxt: UITextField!
+    
+    @IBOutlet weak var LocationTxt: UITextField!
+    
+    @IBOutlet weak var AgeTxt: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
