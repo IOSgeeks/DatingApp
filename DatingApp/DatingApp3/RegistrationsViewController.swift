c66ba15755545db0d80cbf1864b19b9f3cd24262
Wildline <Wildlinelincifort11234@gmail.com>
2021-11-19 00:59:41 -0500

    Update

--- DatingApp/DatingApp3/RegistrationsViewController.swift
+++ DatingApp/DatingApp3/RegistrationsViewController.swift
@@ -28,9 +28,32 @@ class RegistrationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        private var datePicker = UIDatePicker()
+        dataPicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(RegistrationsViewController.dateChanged(datePicker:)), for: .valueChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(RegistrationsViewController.viewTapped(gestureRecognizer:)))
+        
+        view.addGestureRecognizer(tapGesture)
+        
+        BirthDateTxt.inputview = datePicker
+        
         // Do any additional setup after loading the view.
     }
-   
+    
+    @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer){
+        view.endEditing(true)
+        
+    }
+    
+    @objc func dateChanged(datePicker: UIDatePicker){
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter. dateFormat = "MM/dd/yyyy"
+        BirthDateTxt.inputview = dateFormatter.string(from: datePicker.date)
+        view.endEditing(true)
+        
+    }
 
     /*
     // MARK: - Navigation
